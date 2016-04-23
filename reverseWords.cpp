1590fa2f345ce1a5dd3f3899f457ac3d56098c39
SkingXQ <skingxq@gmail.com>
2016-04-23 08:36:42 -0400

    reverseWords.cpp

---
+++ reverseWords.cpp
@@ -0,0 +1,38 @@
+/*
+	becare for the situation:
+	1. ""
+	2. "   "
+	3."sking is     blue"
+        4."sking is blue  "or "   sking is blue"
+*/
+class Solution {
+public:
+    void reverseWords(string &s) {
+        if(s.length() == 0) return;
+        int begin=0;
+        while(*(s.end()-1) == ' '){
+            s.erase(s.length()-1, 1); 
+        }   
+        for(int i=0; i < s.length();){
+            if(s[i] == ' '){
+                if(begin == i){ 
+                    s.erase(begin, 1); 
+                }else{
+                    rever(s, begin, i-1);
+                    i++;
+                    begin = i;
+                }   
+            }else{
+                 i++;
+            }   
+        }   
+        rever(s, begin, s.length()-1);
+        rever(s, 0, s.length()-1);
+    }   
+  
+    void rever(string &s, int begin, int end){
+        while(begin < end){
+            swap(s[begin++], s[end--]);
+        }   
+     }    
+  };  
