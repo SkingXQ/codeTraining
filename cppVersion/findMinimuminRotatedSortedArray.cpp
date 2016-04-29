37bdaf40bbe27b1866b54d55ef92c9bc8a9ce2a3
SkingXQ <skingxq@gmail.com>
2016-04-29 06:25:46 -0400

    findMinimuminRotatedSortedArray

---
+++ cppVersion/findMinimuminRotatedSortedArray.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        if(nums.size() == 1) return nums[0];
+        int n = nums.size() - 1;
+        return divide(nums, 0, n);
+    }
+    
+    int divide(vector<int>& nums, int left, int right){
+        if(right - left == 1){
+            if(nums[right]<nums[left])
+                return nums[right];
+            else
+                return nums[left];
+        }
+        if(nums[(left+right)/2] > nums[right] && nums[(left+right)/2] > nums[left]){
+            return divide(nums, (left+right)/2, right);}
+        else if(nums[(left+right)/2] < nums[right] && nums[(left+right)/2] < nums[left]){
+            return divide(nums, left, (left+right)/2);}
+        else if(nums[(left+right)/2] < nums[right] && nums[(left+right)/2] >= nums[left]){
+            return nums[left];}
+    }
+};
