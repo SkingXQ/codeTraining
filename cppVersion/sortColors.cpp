be659f6e937ad6c5f4f1a2b8f48a45dcfeab4eee
SkingXQ <skingxq@gmail.com>
2016-06-19 08:46:50 -0400

    sortColors.cpp

---
+++ cppVersion/sortColors.cpp
@@ -0,0 +1,67 @@
+
+/*
+    Link: https://leetcode.com/problems/sort-colors/
+    Tips: it similar with first missing number
+          0 move the end of substring of 0's
+          2 move to the begin og substring of 2's
+          in the end It sorted.
+	  Please remember some key points: over boundary and how to give the number of i.(i speend here 2 and half hours)
+          I think I shoule have better ways to solve it.
+*/
+#include<vector>
+#include<iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        if(!nums.size() || nums.size() == 1) return;
+        int zero = 0;
+        for(; zero < nums.size(); zero++) {
+            if(nums[zero] != 0) break;
+        }
+        int two = nums.size() - 1;
+        for(; two > 0; two--) {
+            if(nums[two] != 2) break;
+        }
+        for(int i=zero; i <= two;) {
+            if(zero > nums.size()-1 || two < 0) return;
+            if(nums[i] == 2) {
+                int t = nums[two];
+                nums[two] = 2;
+                nums[i] = t;
+                while(nums[two] == 2) {
+                    two--;
+                    if(two < 0) return;
+                }
+             }
+            if(nums[i] == 0) {
+                int t = nums[zero];
+                nums[zero] = 0;
+                nums[i] = t;
+                while(nums[zero] == 0) {
+                    zero++;
+                    if(zero > nums.size()-1) return;
+                }
+                i = zero;
+            }else{
+                i++;
+            }
+        }
+        for(auto n :nums)
+            cout<<n<<" ";
+        cout<<endl;
+    }
+};
+
+int main()
+{
+    Solution a;
+//    vector<int> b = {2,2,0,0,2,0,2,1,0};
+    vector<int> c = {0,0,0,1,2,2,1,2,0,1};
+  //  a.sortColors(b);
+    a.sortColors(c);
+    //a.sortColors(vector<int>{1,0});
+    //a.sortColors(vector<int>{2,0});
+}
